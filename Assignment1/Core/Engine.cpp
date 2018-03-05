87c1732f81f32d6258259f011b4da966e5fe706d
Wraine <bwallis24@gmail.com>
2018-03-05 17:35:06 -0500

    Summary

---
+++ Assignment1/Core/Engine.cpp
@@ -0,0 +1,20 @@
+#include "Engine.h"
+#include <stdio.h>
+
+
+
+Engine::Engine()
+{
+}
+
+
+Engine::~Engine()
+{
+}
+
+void Engine::print()
+{
+	printf("I don't know what I'm supposed to be typing here");
+
+	getchar();
+}
